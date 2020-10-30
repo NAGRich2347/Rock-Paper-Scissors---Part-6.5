680c35fd58301faa27852afc9edf839705d020de
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-10-29 23:14:49 -0400

    Add project files.

---
+++ Rock, Paper, Scissors - Part 6.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rock__Paper__Scissors___Part_6._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
